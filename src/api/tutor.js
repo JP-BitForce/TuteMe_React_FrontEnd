69a0b520de6abd9e39ffd4f9b689a2abac23e0ee
pragashraj <rajpragashraj30@gmail.com>
2021-05-05 10:05:47 +0530

    feat: search & filter tutors

--- src/api/tutor.js
+++ src/api/tutor.js
@@ -1,4 +1,4 @@
-import { GET } from './core';
+import { GET, POST } from './core';
 
 export const getEndpointWithPrefix = (endpoint) => {
     return `tutor/profile/${endpoint}`
@@ -7,4 +7,14 @@ export const getEndpointWithPrefix = (endpoint) => {
 export const getTutors = (token, page) => {
     const endpoint = getEndpointWithPrefix(`getAll/${page}`)
     return GET(endpoint, token)
+}
+
+export const searchTutorByValue = (token, page, value) => {
+    const endpoint = getEndpointWithPrefix(`search_tutor_by_value?value=${value}&page=${page}`)
+    return GET(endpoint, token)
+}
+
+export const filterTutorsByCategories = (token, body) => {
+    const endpoint = getEndpointWithPrefix(`filter_tutor_by_category`)
+    return POST(endpoint, body, token)
 }
