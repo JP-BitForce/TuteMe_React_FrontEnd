4feb1dc0d0509817b648023f867fca1e4b0aed69
pragashraj <rajpragashraj30@gmail.com>
2021-05-08 18:29:33 +0530

    feat: real time chat[ongoing]

--- src/screens/My Chat/MyChat.js
+++ src/screens/My Chat/MyChat.js
@@ -29,14 +29,27 @@ import avatar_2 from '../../assets/images/shared/avatar_2.jpg'
 import avatar_3 from '../../assets/images/shared/avatar_3.jpg'
 import './Chat.css'
 
+var stompClient = null;
+
 class MyChat extends Component {
     state = {
-        loading: false,
+        loading: true,
         leftPanelOpen: true,
         message:"",
         search: "",
         recipients: "",
-        currentPersonnel: null
+        currentPersonnel: null,
+        broadcastMessages: [],
+        username: "spr",
+        mobileView: false,
+        channelConnected: false,
+        chatMessage: '',
+        roomNotification: [],
+        error: null,
+        bottom: false,
+        curTime: '',
+        openNotifications: false,
+        bellRing: false,
     }
 
     dummayPeople = ["Alice Walker", "Chris Haris", "Ron Weasley", "Bob Watson"]
@@ -44,6 +57,134 @@ class MyChat extends Component {
         {title: "Zack Zimmer", lastText:"Hi ...", type: "individual", time: "13 minutes", src : avatar_2},
         {title: "Javascript-Team", lastText:"Script....", type: "group", time: "13 minutes", src : avatar_3},
     ]
+    dummyMessages = [
+        {message: "hi", sender: "spr", dateTime: "15:25"},
+        {message: "hello there", sender: "someone", dateTime: "15:27"},
+        {message: "how are u?", sender: "spr", dateTime: "15:30"},
+        {message: "im fine, and u?", sender: "someone", dateTime: "15:31"},
+    ]
+
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this))
+        this.resize()
+
+        this.setState({ curTime: new Date().toLocaleString() })
+        this.timerID = setInterval(() => this.state.bellRing ? this.setState({ bellRing: false }) : "", 10000)
+        this.connect()
+    }
+    
+    resize() {
+        let mobileView = (window.innerWidth <= 850)
+        this.setState({ mobileView: mobileView })
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resize.bind(this))
+    }
+
+    componentDidUpdate() {
+        if (!this.state.error) {
+            this.scrollToBottom()
+        }
+    }
+
+    connect = () => {
+        const Stomp = require('stompjs')
+        var SockJS = require('sockjs-client')
+        SockJS = new SockJS('http://localhost:8080/ws')
+        stompClient = Stomp.over(SockJS);
+        stompClient.connect({}, this.onConnected, this.onError);
+    }
+
+    onConnected = () => {
+        this.setState({ channelConnected: true, loading: false, error: null })
+        // Subscribing to the public topic
+        stompClient.subscribe('/topic/pubic', this.onMessageReceived)
+        // Registering user to server as a public chat user
+        stompClient.send("/ws-app/addUser", {}, JSON.stringify({ sender: this.state.username, type: 'JOIN' }))
+    }
+    
+    sendMessage = (type) => {
+        const {message, username} = this.state
+        if (stompClient) {
+          var chatMessage = {
+            sender: username,
+            content: message,
+            type: type
+          }
+          // send public message
+          stompClient.send("/ws-app/sendMessage", {}, JSON.stringify(chatMessage))
+        }
+    }
+
+    onMessageReceived = (payload) => {
+        const {roomNotification, broadcastMessages} = this.state
+        var message = JSON.parse(payload.body)
+        if (message.type === 'JOIN') {
+            roomNotification.push({ 'sender': message.sender + " ~ joined", 'status': 'online', 'dateTime': message.dateTime })
+            this.setState({
+                roomNotification: roomNotification,
+                bellRing: true
+            })
+        }
+        else if (message.type === 'LEAVE') {
+            roomNotification.map((notification, i) => {
+                if (notification.sender === message.sender + " ~ joined") {
+                    notification.status = "offline"
+                    notification.sender = message.sender + " ~ left"
+                    notification.dateTime = message.dateTime
+                }
+                return 1
+            })
+            this.setState({
+                roomNotification: roomNotification,
+                bellRing: true
+            })
+        }
+        else if (message.type === 'TYPING') {
+            roomNotification.map((notification, i) => {
+                if (notification.sender === message.sender + " ~ joined") {
+                    if (message.content)
+                        notification.status = "typing..."
+                    else
+                        notification.status = "online"
+                }
+                return 1
+            })
+            this.setState({
+                roomNotification: roomNotification
+            })
+        }
+        else if (message.type === 'CHAT') {
+            roomNotification.map((notification, i) => {
+                if (notification.sender === message.sender + " ~ joined") {
+                    notification.status = "online"
+                }
+                return 1
+            })
+            broadcastMessages.push({
+                message: message.content,
+                sender: message.sender,
+                dateTime: message.dateTime
+            })
+            this.setState({
+                broadcastMessages: broadcastMessages,
+            })
+        }
+    }
+
+    onError = (error) => {
+        this.setState({
+          error: 'Could not connect you to the Chat Room Server. Please refresh this page and try again!',
+          loading: false
+        })
+    }
+
+    scrollToBottom = () => {
+        var object = this.refs.messageBox;
+        if (object)
+        object.scrollTop = object.scrollHeight;
+    }
 
     handleSearchOnChange = (event) => {
         this.setState({
@@ -53,7 +194,7 @@ class MyChat extends Component {
 
     handleMessageOnType = (event) => {
         this.setState({
-            message:event.target.value
+            message: event.target.value
         })
     }
 
@@ -95,10 +236,6 @@ class MyChat extends Component {
 
     }
 
-    handleSendOnClick = () => {
-
-    }
-
     renderChatListTop = () => {
         return (
             <div className = "chat_list_top">
@@ -130,7 +267,7 @@ class MyChat extends Component {
     renderSearchField = (label) => {
         return (
             <TextField 
-                id="input-with-icon-adornment"
+                id = {`input-with-icon-adornment_${label}`}
                 label= {label}
                 variant="outlined" 
                 onChange = {this.handleSearchOnChange}
@@ -190,7 +327,7 @@ class MyChat extends Component {
         )
     }
 
-    renderMessangerTop_search = () => {
+    renderMessangerTopSearch = () => {
         return (
             <div className = "messanger_top_search">
                 <h6>To:</h6>
@@ -199,7 +336,7 @@ class MyChat extends Component {
         )
     }
 
-    renderMessangerTop_item = () => {
+    renderMessangerTopItem = () => {
         const currentPersonnel = this.state.currentPersonnel
         return (
             <div className = "messanger_top_item">
@@ -224,37 +361,92 @@ class MyChat extends Component {
         )
     }
 
+    renderMainChatFooter = () => {
+        return (
+            <div className = "main_chat__footer">
+                <div className = "main_input_base">
+                    <div className = "icon_button" onClick = {this.handleEmojiOnClick}>
+                        <InsertEmoticon/>
+                    </div>
+                    <TextField
+                        id="outlined-margin-normal"
+                        placeholder="write something...."
+                        fullWidth
+                        InputLabelProps={{ shrink: true }}
+                        variant="outlined"
+                        size="small"
+                        onChange = {this.handleMessageOnType}
+                    />
+                    <div className = "icon_button" onClick = {this.handleAttachmentOnClick}>
+                        <AttachFile/>
+                    </div>
+                    <div className = "icon_button" onClick = {this.handleMicOnClick}>
+                        <Mic/>
+                    </div>
+                </div>
+                <div className = "send_base">
+                    <div className = "icon_button" onClick = {()=> this.sendMessage('CHAT')}>
+                        <Send/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    renderMyMessage = (msg, i) => {
+        return (
+            <li className = "chat_list_item_mine" key={i}>
+                <div>
+                    <div className = "message_box_mine">
+                        {msg.message}
+                    </div>
+                    <div className = "message_dateTime">
+                        {msg.dateTime}
+                    </div>
+                </div>
+            </li>
+        )
+    }
+
+    renderSenderMessage = (msg, i) => {
+        return (
+            <li className = "chat_list_item_other" key={i}>
+                <div className = "chat_list_item_other_box">
+                    <div className = "message_box_other">
+                        {msg.message}
+                    </div>
+                    <div className = "message_dateTime">
+                        {msg.dateTime}
+                    </div>
+                </div>
+            </li>
+        )
+    }
+
+    renderMainChatList = () => {
+        const {username} = this.state
+        return (
+            <div className = "main_chat_lists">
+                <ul id="chat" ref="messageBox">
+                    {
+                        this.dummyMessages.map((msg, i) => {
+                            if (username === msg.sender) {
+                                return this.renderMyMessage(msg, i)
+                            } else {
+                                return this.renderSenderMessage(msg, i)
+                            }
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+
     renderMainChatContainer = () => {
         return (
             <div className = "main_chat_container">
-                <div className = "main_chat_lists"></div>
-                <div className = "main_chat__footer">
-                    <div className = "main_input_base">
-                        <div className = "icon_button" onClick = {this.handleEmojiOnClick}>
-                            <InsertEmoticon/>
-                        </div>
-                        <TextField
-                            id="outlined-margin-normal"
-                            placeholder="write something...."
-                            fullWidth
-                            InputLabelProps={{ shrink: true }}
-                            variant="outlined"
-                            size="small"
-                            onChange = {this.handleMessageOnType}
-                        />
-                        <div className = "icon_button" onClick = {this.handleAttachmentOnClick}>
-                            <AttachFile/>
-                        </div>
-                        <div className = "icon_button" onClick = {this.handleMicOnClick}>
-                            <Mic/>
-                        </div>
-                    </div>
-                    <div className = "send_base">
-                        <div className = "icon_button" onClick = {this.handleSendOnClick}>
-                            <Send/>
-                        </div>
-                    </div>
-                </div>
+                { this.renderMainChatList() }
+                { this.renderMainChatFooter() }
             </div>
         )
     }
@@ -289,9 +481,9 @@ class MyChat extends Component {
                 <div className = "chat_messanger">
                     { 
                         currentPersonnel ?
-                        this.renderMessangerTop_item()
+                        this.renderMessangerTopItem()
                         :
-                        this.renderMessangerTop_search() 
+                        this.renderMessangerTopSearch() 
                     }
                     <Divider/>
                     { this.renderMainChatContainer() }
@@ -301,6 +493,7 @@ class MyChat extends Component {
     }
 
     render() {
+        const {loading, channelConnected, error} = this.state
         return (
             <div className = "my_chat_root">
                 <HeaderTopper
@@ -309,10 +502,11 @@ class MyChat extends Component {
                     childNav = {["Chat"]}
                 />
                 {
-                    this.state.loading ?
-                    <Loading/>
+                    loading ? <Loading open = {loading}/>
                     :
-                    this.renderChat()
+                    channelConnected ? this.renderChat()
+                    :
+                    error && <span className = "error">{error}</span>
                 }
             </div>
         )
