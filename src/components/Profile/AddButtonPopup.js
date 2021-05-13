86c1e5d4f6d1d3fd435601f18bf6766cf4b06bec
Piruntha <spiruntha1116@gmail.com>
2021-05-13 19:31:49 +0530

    modify some codes

--- src/components/Profile/AddButtonPopup.js
+++ src/components/Profile/AddButtonPopup.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import { Dialog, DialogTitle, DialogContent, makeStyles, Typography, Button } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import AddMyInterest from '../../screens/Profile/MyInterestCard/AddMyInterestForm';
 import DialogActions from '@material-ui/core/DialogActions';
+import AddMyInterest from "../../screens/Profile/MyInterestCard/AddMyInterestForm";
 
 const useStyles = makeStyles(theme => ({
     dialogWrapper: {
@@ -15,7 +15,7 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-export default function AddButtonPopup(props) {
+function AddButtonPopup(props) {
     const [selectData, setSelectData] = useState([]);
     const { title, openPopup, setOpenPopup } = props;
     const classes = useStyles();
@@ -25,38 +25,41 @@ export default function AddButtonPopup(props) {
         selectdata : selectData,
         setselectdata : setSelectData
     }
+
     const handleAdd = () => {
         props.addData(selectData);
         setSelectData([]);
         setOpenPopup(false);
     }
+
     const checkSkills = (title) => {
         switch (title) {
-            case 'Add Subjects': 
-            return title
+            case 'Add Subjects':
+                return title
 
-            case 'Add Top Skills': 
-            return title
+            case 'Add Top Skills':
+                return title
 
-            case 'Add Tools & Technologies': 
-            return title
+            case 'Add Tools & Technologies':
+                return title
 
-            default: 
-            return title
+            default:
+                return title
         }
     }
+
     return (
         <Dialog open={openPopup} maxWidth="md" classes={{ paper: classes.dialogWrapper }}>
             <DialogTitle className={classes.dialogTitle}>
-                <div style={{ display: 'flex' }}>
-                    <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>    
+                <div style={{display: 'flex'}}>
+                    <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
                         {checkSkills(title)}
                     </Typography>
                     <Button
                         onClick={()=>{
                             setSelectData([])
                             setOpenPopup(false)
-                            }}>
+                        }}>
                         <CloseIcon />
                     </Button>
                 </div>
@@ -65,10 +68,12 @@ export default function AddButtonPopup(props) {
                 <AddMyInterest {...config} />
             </DialogContent>
             <DialogActions>
-          <Button onClick={handleAdd} style={{color:"white",backgroundColor:"#2578F5", margin:10}}>
-            Add
-          </Button>
-        </DialogActions>
+                <Button onClick={handleAdd} style={{color:"white",backgroundColor:"#2578F5", margin:10}}>
+                    Add
+                </Button>
+            </DialogActions>
         </Dialog>
     )
 }
+
+export default React.memo(AddButtonPopup);
