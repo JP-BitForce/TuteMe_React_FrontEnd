0be3e51448149443385e631bf0576ac4889d4bc0
Piruntha <spiruntha1116@gmail.com>
2021-05-13 20:21:26 +0530

    modify  key values in array

--- src/components/Profile/CustomComboBox.js
+++ src/components/Profile/CustomComboBox.js
@@ -2,76 +2,70 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
-
 function CustomComboBox(props) {
-  const {title} = props
-  
-  const [value, setValue] = React.useState('');
+    const {title} = props;
+    const [value, setValue] = React.useState('');
 
-  const selectSkills = (newValue) => {
-   setValue(newValue);
-   if (newValue !== null){
-     props.selectData(newValue)
-   }
-  }
-
-  const checkSkills = (title) => {
-   
-    switch (title) {
-        case 'Add Subjects': 
-        return subjectArray
-
-        case 'Add Top Skills': 
-        return topSkillsArray
-
-        case 'Add Tools & Technologies': 
-        return toolsArray
-
-        default: 
-        return subjectArray
+    const selectSkills = (newValue) => {
+        setValue(newValue);
+        if (newValue !== null){
+            props.selectData(newValue)
+        }
     }
-}
-  return (
-      <div>
-        <Autocomplete
-            value={value}
-            onChange={(event, newValue) => {
-              selectSkills(newValue)
-            }}
-            id="controllable-states-demo"
-            options={checkSkills(title).map((option) => option.title)}
-            style={{ width: "90%", margin:10 }}
-            renderInput={(params) => <TextField {...params} label="skills" variant="outlined" />}
-        />
-      </div>
-  );
+
+    const checkSkills = (title) => {
+        switch (title) {
+            case 'Add Subjects':
+                return subjectArray
+
+            case 'Add Top Skills':
+                return topSkillsArray
+
+            case 'Add Tools & Technologies':
+                return toolsArray
+
+            default:
+                return subjectArray
+        }
+    }
+    return (
+        <div>
+            <Autocomplete
+                value={value}
+                onChange={(event, newValue) => {
+                    selectSkills(newValue)
+                }}
+                id="controllable-states-demo"
+                options={checkSkills(title).map((option) => option.title)}
+                style={{ width: "90%", margin:10 }}
+                renderInput={(params) => <TextField {...params} label="skills" variant="outlined" />}
+            />
+        </div>
+    );
 }
 
 export default React.memo(CustomComboBox);
 
-// filter data
 
 const subjectArray = [
-  { title: 'Mathematics', key: 0 },
-  { title: 'Science', key: 1 },
-  { title: 'History', key: 2 },
-  { title: 'Physics', key: 3 },
-  { title: 'Chemistry', key: 4 },
-  { title: "Biology", key: 5 },
+    { title: 'Mathematics', key: 0 },
+    { title: 'Science', key: 1 },
+    { title: 'History', key: 2 },
+    { title: 'Physics', key: 3 },
+    { title: 'Chemistry', key: 4 },
+    { title: "Biology", key: 5 },
 ];
 
 const topSkillsArray = [
- 
-  { title: 'leadership',key: 7 },
-  { title: 'time management', key: 8 },
-  { title: 'communication skill', key: 9 },
- 
+    { title: 'leadership',key: 0 },
+    { title: 'time management', key: 1 },
+    { title: 'communication skill', key: 2 },
 ];
 
 const toolsArray = [
-  { title: 'React', key: 9 },
-  { title: 'Java', key: 10 },
-  { title: 'JavaScript', key: 11 },
-  { title: 'Phython', key: 12 },
-  { title: 'Machine learning', key: 13 },
+    { title: 'React', key: 0 },
+    { title: 'Java', key: 1 },
+    { title: 'JavaScript', key: 2 },
+    { title: 'Phython', key: 3 },
+    { title: 'Machine learning', key: 4 },
 ];
